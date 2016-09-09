be199f29aea1c45af79f1a8bc9d9d2ae12f08144
Wörsty <git@loxberry.woerstenfeld.de>
2016-09-09 21:37:41 +0200

    Path replacement

--- postinstall.sh
+++ postinstall.sh
@@ -17,6 +17,7 @@ ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 
 /bin/sed -i "s/REPLACEBYSUBFOLDER/$ARGV3/" $ARGV5/config/plugins/$ARGV3/ble_scanner.cfg
 /bin/sed -i "s/REPLACEBYSUBFOLDER/$ARGV3/" $ARGV5/system/daemons/plugins/$ARGV2
+/bin/sed -i "s/REPLACEBYBASEFOLDER/$ARGV5/" $ARGV5/system/daemons/plugins/$ARGV2
 /bin/sed -i "s/REPLACEBYNAME/$ARGV2/" $ARGV5/config/plugins/$ARGV3/ble_scanner.cfg
 
 
